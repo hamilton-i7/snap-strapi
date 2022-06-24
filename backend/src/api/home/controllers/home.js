1e4f194b8d24c4fbe2eca20dd5c4d7168563ed56
Juan Hamilton <hedwards507@gmail.com>
2022-06-24 09:11:55 -0500

    populated home response

--- backend/src/api/home/controllers/home.js
+++ backend/src/api/home/controllers/home.js
@@ -1,9 +1,35 @@
-'use strict';
+"use strict";
 
 /**
  *  home controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::home.home');
+module.exports = createCoreController("api::home.home", ({ strapi }) => ({
+  async find(ctx) {
+    const { query } = ctx;
+
+    const entity = await strapi.entityService.findMany("api::home.home", {
+      ...query,
+      populate: {
+        menu: {
+          populate: {
+            logo: true,
+            links: {
+              populate: {
+                icon: true,
+              },
+            },
+          },
+        },
+        clients: true,
+        imageDesktop: true,
+        imageMobile: true,
+      },
+    });
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
