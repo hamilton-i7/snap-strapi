8dcb7e845e5a26965e86fbded9a70a7e39665091
Juan Hamilton <hedwards507@gmail.com>
2022-07-02 14:19:29 -0500

    Change content security policy

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -4,24 +4,15 @@ module.exports = ({ env }) => [
     name: "strapi::security",
     config: {
       contentSecurityPolicy: {
-        useDefaults: true,
         directives: {
-          "connect-src": ["'self'", "https:"],
+          "script-src": ["'self'", "'unsafe-inline'", "cdn.jsdelivr.net"],
           "img-src": [
             "'self'",
             "data:",
-            "blob:",
-            "dl.airtable.com",
-            "yourBucketName.s3.yourRegion.amazonaws.com",
+            "cdn.jsdelivr.net",
+            "strapi.io",
+            `${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
           ],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            "yourBucketName.s3.yourRegion.amazonaws.com",
-          ],
-          upgradeInsecureRequests: null,
         },
       },
     },
