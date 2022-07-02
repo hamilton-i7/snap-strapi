b9e62286ebe3d74e5d27b769b3040bcbb3e56668
Juan Hamilton <hedwards507@gmail.com>
2022-07-02 14:01:06 -0500

    Add config vars to Heroku

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -3,6 +3,7 @@ module.exports = ({ env }) => ({
     config: {
       provider: "strapi-provider-upload-aws-s3",
       providerOptions: {
+        // Credentials
         accessKeyId: env("AWS_ACCESS_KEY_ID"),
         secretAccessKey: env("AWS_ACCESS_SECRET"),
         region: env("AWS_REGION"),
