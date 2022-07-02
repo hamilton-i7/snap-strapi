0036f82dc37c87bbf3e07a0a75435a8ffdb7e0dc
Juan Hamilton <hedwards507@gmail.com>
2022-07-02 14:07:55 -0500

    Fix provider name bug

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,9 +1,8 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: "strapi-provider-upload-aws-s3",
+      provider: "aws-s3",
       providerOptions: {
-        // Credentials
         accessKeyId: env("AWS_ACCESS_KEY_ID"),
         secretAccessKey: env("AWS_ACCESS_SECRET"),
         region: env("AWS_REGION"),
